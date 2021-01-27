2109af191cd501d4c5210d263275c574e347a127
rafaelcnagy <rafaelcnagy@hotmail.com>
2021-01-26 21:19:01 -0300

    Add página de resultados

--- main.py
+++ main.py
@@ -29,11 +29,10 @@ def search(ts_description):
     driver = initialize_driver()
 
     for char_name in char_name_list:
-
         char = tibiadata.search_char(char_name)
         char = guildstats.search_date(char, driver)
         char = tibiaring.search_char(char, driver)
         char_list.append(char)
 
     driver.close()
-    return ''
+    return char_list
