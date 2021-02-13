24f954dc02466f6664a4159331bd7fa1e05cddfd
rafaelcnagy <rafaelcnagy@hotmail.com>
2021-02-12 22:29:43 -0300

    Ajustes configurações

--- EasyHigh/config.py
+++ EasyHigh/config.py
@@ -7,5 +7,5 @@ config.read(os.path.join(os.path.dirname(__file__), '../config.ini'))
 STEAM_ACCOUNT = config['steam']['account_name']
 STEAM_PASSWORD = config['steam']['password']
 
-driver_path = config['selenium']['driver_path'] if config['selenium']['driver_path'].strip() else 'chromedriver.exe'
+driver_path = config['selenium']['driver_path'] if config['selenium']['driver_path'].strip() else '../chromedriver'
 SELENIUM_PATH = os.path.join(os.path.dirname(__file__), driver_path)
